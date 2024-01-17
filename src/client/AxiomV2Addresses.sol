a47788e478790a5210665537078215f302802e4d
Yi Sun <yi-sun@users.noreply.github.com>
2024-01-17 11:22:31 -0500

    feat: update with production AxiomV2Core, AxiomV2Query, AxiomV2CoreMock, and AxiomV2QueryMock addresses

--- src/client/AxiomV2Addresses.sol
+++ src/client/AxiomV2Addresses.sol
@@ -1,20 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {
-    MAINNET_CHAIN_ID, GOERLI_CHAIN_ID, SEPOLIA_CHAIN_ID
-} from "../libraries/configuration/AxiomV2Configuration.sol";
+import { MAINNET_CHAIN_ID, SEPOLIA_CHAIN_ID } from "../libraries/configuration/AxiomV2Configuration.sol";
 
 /// @title AxiomV2Addresses
 /// @notice AxiomV2Addresses is a library that contains the addresses of deployed Axiom V2 contracts
 library AxiomV2Addresses {
-    address public constant AXIOM_V2_CORE_ADDRESS = address(0);
-    address public constant AXIOM_V2_QUERY_ADDRESS = address(0);
+    address public constant AXIOM_V2_CORE_ADDRESS = 0x69963768F8407dE501029680dE46945F838Fc98B;
+    address public constant AXIOM_V2_QUERY_ADDRESS = 0x83c8c0B395850bA55c830451Cfaca4F2A667a983;
 
-    address public constant SEPOLIA_AXIOM_V2_CORE_HISTORICAL_MOCK_ADDRESS = 0xbCBa296B560FA58c15eC220df1353B8Ab8c7A419;
-    address public constant SEPOLIA_AXIOM_V2_CORE_MOCK_ADDRESS = 0xF57252Fc4ff36D8d10B0b83d8272020D2B8eDd55;
-    address public constant SEPOLIA_AXIOM_V2_QUERY_ADDRESS = 0xD4E7469fdB3cAe2C85db4dacD44cb974757CbeD9;
-    address public constant SEPOLIA_AXIOM_V2_QUERY_MOCK_ADDRESS = 0x56A380e544606ad713A8A19BAc1903aa7C2017FF;
+    address public constant SEPOLIA_AXIOM_V2_CORE_MOCK_ADDRESS = 0x69963768F8407dE501029680dE46945F838Fc98B;
+    address public constant SEPOLIA_AXIOM_V2_QUERY_MOCK_ADDRESS = 0x83c8c0B395850bA55c830451Cfaca4F2A667a983;
 
     /// @dev Error returned if the corresponding Axiom V2 contract does not exist for the requested chainId
     error ContractDoesNotExistForChainId();
@@ -28,8 +24,6 @@ library AxiomV2Addresses {
     function axiomV2QueryAddress(uint64 chainId) public pure returns (address addr) {
         if (chainId == MAINNET_CHAIN_ID) {
             addr = AXIOM_V2_QUERY_ADDRESS;
-        } else if (chainId == SEPOLIA_CHAIN_ID) {
-            addr = SEPOLIA_AXIOM_V2_QUERY_ADDRESS;
         } else {
             revert ContractDoesNotExistForChainId();
         }
@@ -60,8 +54,6 @@ library AxiomV2Addresses {
     function axiomV2CoreAddress(uint64 chainId) public pure returns (address addr) {
         if (chainId == MAINNET_CHAIN_ID) {
             addr = AXIOM_V2_CORE_ADDRESS;
-        } else if (chainId == SEPOLIA_CHAIN_ID) {
-            addr = SEPOLIA_AXIOM_V2_CORE_HISTORICAL_MOCK_ADDRESS;
         } else {
             revert ContractDoesNotExistForChainId();
         }
